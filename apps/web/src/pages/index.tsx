4defa034678617aeafa3b6d3866fbd6f3c224437
yoshikazu hotta <hotta-nagoya-6163@au.com>
2022-12-04 11:21:48 +0900

    :lipstick: fetch users in top page

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,9 +1,24 @@
-import { NextPage } from 'next';
+import { InferGetStaticPropsType, NextPage } from 'next';
+import { getUsers } from 'dao';
 
-const Page: NextPage = () => {
+type Props = InferGetStaticPropsType<typeof getStaticProps>;
+
+export const getStaticProps = async () => {
+  const users = await getUsers();
+  return {
+    props: {
+      users,
+    },
+  };
+};
+
+const Page: NextPage<Props> = ({ users }) => {
   return (
     <div>
       <h1>Web</h1>
+      {users.map((user) => (
+        <p>{`${user.id}: ${user.name}(${user.age})`}</p>
+      ))}
     </div>
   );
 };
